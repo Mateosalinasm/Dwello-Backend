4ce563b746f39464b49ec3cb81114da5cfd8708c
Mateosalinasm <mateo.salinas95@gmail.com>
2023-06-07 12:39:09 -0500

    moved routes to index.js to be able to deploy to vercel
    Signed-off-by: Mateosalinasm <mateo.salinas95@gmail.com>

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-const propertyRoutes = require('./routes/propertyRoutes.js')
+// const propertyRoutes = require('./routes/propertyRoutes.js')
 const dotenv = require('dotenv')
 dotenv.config()
 
@@ -12,12 +12,23 @@ const PORT = process.env.PORT || 3003
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use('/dwellings', propertyRoutes)
+// app.use('/dwellings', propertyRoutes)
 
 
 app.get('/', (req, res) => {
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' })
 })
+
+const { propertiesCtrls } = require('./controllers')
+
+//ROUTES - METHODS //
+app.get('/dwellings', propertiesCtrls.getProperties)
+app.get('/dwellings/:id', propertiesCtrls.getPropertyById)
+app.post('/dwellings', propertiesCtrls.createProperty)
+app.post('/dwellings/seed', propertiesCtrls.seedProperty)
+app.put("/dwellings/:id", propertiesCtrls.updateProperty)
+app.delete('/:id', propertiesCtrls.deleteProperty)
+
 
 app.listen(PORT, () => {
     console.log('Listening on port ', PORT)
