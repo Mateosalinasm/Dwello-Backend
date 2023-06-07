9cddf8f4c1119e3a5698361fbc04aba05c2f5fff
Mateosalinasm <mateo.salinas95@gmail.com>
2023-06-07 18:45:23 -0500

    trying to get data to fetch from the backend

--- api/index.js
+++ api/index.js
@@ -5,29 +5,28 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const app = express()
-
 const PORT = process.env.PORT || 3003
 
 //Middleware
-app.use(cors())
+app.use(cors({ origin: '*' }))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 // app.use('/dwellings', propertyRoutes)
 
 
-app.get('/', (req, res) => {
+app.get('/api/', (req, res) => {
     res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' })
 })
 
 const { propertiesCtrls } = require('./controllers')
 
 //ROUTES - METHODS //
-app.get('/dwellings', propertiesCtrls.getProperties)
-app.get('/dwellings/:id', propertiesCtrls.getPropertyById)
-app.post('/dwellings', propertiesCtrls.createProperty)
-app.post('/dwellings/seed', propertiesCtrls.seedProperty)
-app.put("/dwellings/:id", propertiesCtrls.updateProperty)
-app.delete('/:id', propertiesCtrls.deleteProperty)
+app.get('api/dwellings', propertiesCtrls.getProperties)
+app.get('api/dwellings/:id', propertiesCtrls.getPropertyById)
+app.post('api/dwellings', propertiesCtrls.createProperty)
+app.post('api/dwellings/seed', propertiesCtrls.seedProperty)
+app.put('api/dwellings/:id', propertiesCtrls.updateProperty)
+app.delete('api/:id', propertiesCtrls.deleteProperty)
 
 
 app.listen(PORT, () => {
--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,6 @@
-const router = require('express').Router()
-const propertyRoutes = require('./propertyRoutes')
+// const router = require('express').Router()
+// const propertyRoutes = require('./propertyRoutes')
 
-router.use('/dwellings', propertyRoutes)
+// router.use('/dwellings', propertyRoutes)
 
-module.exports = router
+// module.exports = router
